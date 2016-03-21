60607904fabb2742f66c51d81e8fd8018c8e39c0
Danny Henn <Danny Henn>
2016-03-20 19:52:03 -0700

    Full directory

---
+++ load_location.py
@@ -0,0 +1,112 @@
+import urllib
+import sqlite3
+import json
+import time
+import ssl
+import re
+
+serviceurl = "http://maps.googleapis.com/maps/api/geocode/json?"
+
+scontext = None
+
+conn = sqlite3.connect('police_killings.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Locations
+    (date TEXT,
+    name TEXT,
+    race TEXT,
+    sex TEXT,
+    armed TEXT,
+    cause TEXT,
+    lat REAL,
+    long REAL,
+    date_index INT,
+    UNIQUE(name, lat, long, date))''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Deaths_Indexed
+    (date TEXT,
+    name TEXT,
+    address TEXT,
+    state TEXT,
+    city TEXT,
+    race TEXT,
+    sex TEXT,
+    armed TEXT,
+    cause TEXT,
+    date_text TEXT,
+    UNIQUE(name, address, date))''')
+
+cur.execute('''SELECT date, name, address, state, city, race, sex, armed, cause from Deaths''')
+data = cur.fetchall()
+months = {'JANUARY':'01', 'FEBRUARY':'02', 'MARCH':'03', 'APRIL':'04', 'MAY': '05', 'JUNE':'06', 'JULY':'07', 'AUGUST':'08', 'SEPTEMBER':'09', 'OCTOBER':'10', 'NOVEMBER':'11', 'DECEMBER':'12'}
+for row in data:
+    datesplit = row[0].split()
+    month = months[datesplit[0]]
+    year = datesplit[2]
+    day = datesplit[1][:-1]
+    if len(day)==1:
+        day = "0" + datesplit[1][:-1]
+    date = year + '-' + month + '-' + day
+    cur.execute('''INSERT OR IGNORE INTO Deaths_Indexed (date, name, address, state, city, race, sex, armed, cause, date_text)
+            VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', ( date, row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[0]) )
+    conn.commit()
+    print date
+
+cur.execute('''SELECT date_text, name, address, state, city, race, sex, armed, cause from Deaths_Indexed order by date(date)''')
+data = cur.fetchall()
+_date = "JANUARY 1, 2015"
+date_index=1
+for row in data:
+    if _date != row[0]:
+        date_index = date_index + 1
+    _date = row[0]
+    _name = row[1]
+    _race = row[5]
+    _sex = row[6]
+    _armed = row[7]
+    _cause = row[8]
+    cur.execute('''SELECT * from Locations
+    where date=:_date and name=:_name and race=:_race and sex=:_sex and armed=:_armed and cause=:_cause''', {"_date": _date, "_name": _name, "_race": _race, "_sex": _sex, "_armed": _armed, "_cause": _cause})
+    try:
+        data = cur.fetchone()[0]
+        print "Found in database "
+        continue
+    except:
+        pass
+    full_address = row[2] + " " + row[4] + " " + row[3]
+    print 'Resolving', full_address
+    try:
+        url = serviceurl + urllib.urlencode({"sensor":"false", "address": full_address})
+        print 'Retrieving', url
+        uh = urllib.urlopen(url, context=scontext)
+        read = uh.read()
+        js = json.loads(str(read))
+        # print json.dumps(js, indent=4, sort_keys=True)  # We print in case unicode causes an error
+        lat = float(js["results"][0]["geometry"]["location"]["lat"])
+        lng = float(js["results"][0]["geometry"]["location"]["lng"])
+    except:
+        continue
+
+    if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') :
+        print '==== Failure To Retrieve ===='
+        print read
+        break
+
+    cur.execute('''INSERT OR IGNORE INTO Locations (date, name, race, sex, armed, cause, lat, long, date_index)
+            VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)''', ( row[0], row[1], row[5], row[6], row[7], row[8], lat, lng, date_index) )
+    conn.commit()
+    time.sleep(1)
+cur.execute('''SELECT date, race, sex, armed, cause, lat, long, date_index from Locations''')
+data = cur.fetchall()
+features = list()
+for row in data:
+    feature = {'date': row[0], 'race': row[1], 'sex': row[2], 'armed': row[3], 'cause': row[4], 'lat': row[5], 'long': row[6], 'date_index': row[7] }
+    # print "Lat: ",row[5],"\t", "Long: ",row[6]
+    features.append(feature)
+# print json.dumps(features, indent=4, sort_keys=True)
+with open('event_points.json', 'w') as outfile:
+    json.dump(features, outfile)
+print "Data Written"
---
+++ pull_2015.py
@@ -0,0 +1,150 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import sqlite3
+
+# Create SQLite Database
+conn = sqlite3.connect('police_killings.sqlite')
+cur = conn.cursor()
+cur.execute('''DROP TABLE IF EXISTS Deaths''')
+cur.execute('''CREATE TABLE Deaths
+    (date TEXT,
+    name TEXT,
+    race TEXT,
+    sex TEXT,
+    age INTEGER,
+    address TEXT,
+    city TEXT,
+    state TEXT,
+    armed TEXT,
+    cause TEXT,
+    department TEXT,
+    UNIQUE(name,address,date))''')
+
+# Initialize data and drivers, navigate to 2015 page
+# rows = list()
+driver = webdriver.Chrome()
+driver.implicitly_wait(15)
+driver.get("http://www.theguardian.com/us-news/ng-interactive/2015/jun/01/the-counted-police-killings-us-database")
+ac1 = ActionChains(driver)
+ac2 = ActionChains(driver)
+ac3 = ActionChains(driver)
+ac4 = ActionChains(driver)
+ac5 = ActionChains(driver)
+button_2015 = driver.find_element_by_xpath('html/body/figure/div[2]/div[2]/div[1]/div[1]/a[2]')
+button_2015.click()
+
+# Scroll up and down the page in order to load all cells
+time.sleep(2)
+for j in range(10):
+    ac1.key_down(Keys.PAGE_DOWN).perform()
+    time.sleep(0.5)
+    ac2.key_up(Keys.PAGE_DOWN).perform()
+time.sleep(1)
+for j in range(10):
+    ac3.key_down(Keys.PAGE_UP).perform()
+    time.sleep(0.25)
+    ac4.key_up(Keys.PAGE_UP).perform()
+time.sleep(1)
+
+# Go through each month and each listing, skipping odd 'months' that erroneously appear and are empty
+months = driver.find_elements_by_css_selector('div.month')
+i=1
+for month in months:
+    # print "@@@@@@@@@@@@@@@@", i, "@@@@@@@@@@@@@@@@"
+    if (i % 2 == 0):
+        i = i + 1
+        continue
+    else:
+        records = month.find_elements_by_css_selector('article.incident')
+        for x in records:
+            link = x.find_element_by_css_selector('div.record')
+            # Ugly try-except statements to try to get this to run quicker
+            try:
+                time.sleep(0.5)
+                link.click()
+            except:
+                try:
+                    time.sleep(1)
+                    link.click()
+                except:
+                    try:
+                        time.sleep(1.5)
+                        link.click()
+                    except:
+                        try:
+                            time.sleep(2)
+                            link.click()
+                        except:
+                            time.sleep(10)
+                            link.click()
+            try:
+                time.sleep(0.5)
+                details = driver.find_element_by_css_selector('article.details-container')
+                info = details.text.split('\n')
+                date = info[0]
+                name = info[1]
+            except:
+                try:
+                    time.sleep(1)
+                    details = driver.find_element_by_css_selector('article.details-container')
+                    info = details.text.split('\n')
+                    date = info[0]
+                    name = info[1]
+                except:
+                    try:
+                        time.sleep(2.5)
+                        details = driver.find_element_by_css_selector('article.details-container')
+                        info = details.text.split('\n')
+                        date = info[0]
+                        name = info[1]
+                    except:
+                        time.sleep(10)
+                        details = driver.find_element_by_css_selector('article.details-container')
+                        info = details.text.split('\n')
+                        date = info[0]
+                        name = info[1]
+            if info[14].startswith('STATUS'): short = 0
+            else: short = 1
+            if info[3].startswith('Asian/Pacific'):
+                race = info[3][:21]
+                sex = info[3][23:].split(',')[0]
+            else:
+                race = info[3].split(' ')[0]
+                sex = info[3].split(' ')[1][:-1]
+            try:
+                age = int(info[3].split(',')[1])
+            except:
+                age = -1
+            if short==1:
+                address = "Unknown"
+                city = info[5].split(',')[0]
+                state = info[5].split(',')[1].strip()
+                armed = info[7]
+                cause = info[9]
+                department = info[11]
+            else:
+                address = info[5]
+                city = info[6].split(',')[0]
+                state = info[6].split(',')[1].strip()
+                armed = info[8]
+                cause = info[10]
+                department = info[12]
+            print "Pass", date
+            e = (date, name, race, sex, age, address, city, state, armed, cause, department)
+            container = details.find_element_by_css_selector("div.details")
+            close = details.find_element_by_css_selector("div.close")
+            try:
+                container.click()
+            except:
+                time.sleep(1)
+                container.click()
+            close.click()
+            # rows.append(e)
+            cur.execute('INSERT OR IGNORE INTO Deaths VALUES (?,?,?,?,?,?,?,?,?,?,?)', e )
+            conn.commit()
+        i = i+1
+driver.close()
+conn.commit()
+cur.close()
